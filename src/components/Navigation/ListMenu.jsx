a8dbf25b3fd375847860dc41381bc0e6caf2fec5
Aram <aram_martin@alumni.brown.edu>
2021-08-13 17:30:05 -0500

    ADD: git repo steps

--- src/components/Navigation/ListMenu.jsx
+++ src/components/Navigation/ListMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { AppBar, Toolbar, Button, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
